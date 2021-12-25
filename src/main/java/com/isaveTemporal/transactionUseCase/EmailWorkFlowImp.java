311e747c8e47670217b8303ec3c5a7b9626d2982
ajikee1 <akeeri1@students.towson.edu>
2021-12-24 20:57:14 -0500

    email use case

--- src/main/java/com/isaveTemporal/transactionUseCase/EmailWorkFlowImp.java
+++ src/main/java/com/isaveTemporal/transactionUseCase/EmailWorkFlowImp.java
@@ -4,35 +4,23 @@ import io.temporal.activity.ActivityOptions;
 import io.temporal.workflow.Async;
 import io.temporal.workflow.Promise;
 import io.temporal.workflow.Workflow;
+
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
+public class EmailWorkFlowImp implements EmailWorkFlow {
 
-/*
-    Demo the ability of temporal to resume execution without restart on recovery
-    For 'n' the workflow with send 'n' emails to the email address 'ajith@yopmail.com'
-    1. Run 'EmailWorker'
-    2. Run 'EmailWorkFlowTrigger'
-    3. Stop the 'EmailWorker'
-    4. Resume the 'EmailWorker'
-
-    The email address 'ajith@yopmail.com' receives 'n' number of emails.
-*/
-
-
-public class EmailWorkFlowImp implements EmailWorkFlow{
-
-    private final  EmailActivity activity = Workflow.newActivityStub(EmailActivity.class, ActivityOptions.newBuilder().setStartToCloseTimeout(Duration.ofSeconds(20)).build());
+    private final EmailActivity activity = Workflow.newActivityStub(EmailActivity.class, ActivityOptions.newBuilder().setStartToCloseTimeout(Duration.ofSeconds(20)).build());
 
     @Override
     public void initiateWorkFlow(String userName, String password, int num) {
 
         List<Promise<Boolean>> promiseList = new ArrayList<>();
 
-        for( int i = 0; i < num; i++){
+        for (int i = 0; i < num; i++) {
             Workflow.sleep(Duration.ofSeconds(30));
-            promiseList.add(Async.function(activity::sendEmail,userName,password, i));
+            promiseList.add(Async.function(activity::sendEmail, userName, password, i));
         }
 
         Promise.allOf(promiseList).get();
--- src/main/java/com/isaveTemporal/transactionUseCase/EmailWorkFlowTrigger.java
+++ src/main/java/com/isaveTemporal/transactionUseCase/EmailWorkFlowTrigger.java
@@ -1,12 +1,23 @@
 package com.isaveTemporal.transactionUseCase;
 
-import com.isaveTemporal.useCase.UseCaseTwo;
 import io.temporal.api.common.v1.WorkflowExecution;
 import io.temporal.client.WorkflowClient;
 import io.temporal.client.WorkflowOptions;
 import io.temporal.serviceclient.WorkflowServiceStubs;
-import io.temporal.worker.Worker;
-import io.temporal.worker.WorkerFactory;
+
+/*
+    Demo the ability of temporal to resume execution without restart on recovery
+    For input 'n', the workflow will send 'n' emails to the email address 'ajith@yopmail.com' every 30 seconds.
+
+    1. Run 'EmailWorker'
+    2. Run 'EmailWorkFlowTrigger': Workflow in 'Running' status
+    3. Stop the 'EmailWorker' after 30 seconds
+    - Check 'ajith@yopmail.com': It receives 1 email
+    - Check 'ajith@yopmail.com' again in 2 minutes: It receives no additional emails
+    4. Resume the 'EmailWorker' after 2 minutes and wait for the workflow to be in 'Completed' status
+    - Check 'ajith@yopmail.com': It received 'n' number of emails. (temporal retains the state of the workflow i.e. '1'
+      email was sent before the worker failed and on worker re-start continues fom that point with no additional emails sent
+*/
 
 public class EmailWorkFlowTrigger {
 
