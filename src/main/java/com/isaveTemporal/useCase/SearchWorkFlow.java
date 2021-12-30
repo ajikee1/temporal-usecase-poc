057e648e5ce4d4515a2f75d73f0f5db488bb60e0
ajikee1 <akeeri1@students.towson.edu>
2021-12-29 21:11:25 -0500

    added workflow search

---
+++ src/main/java/com/isaveTemporal/useCase/SearchWorkFlow.java
@@ -0,0 +1,146 @@
+package com.isaveTemporal.useCase;
+
+import io.temporal.activity.Activity;
+import io.temporal.activity.ActivityInterface;
+import io.temporal.activity.ActivityOptions;
+import io.temporal.api.common.v1.WorkflowExecution;
+import io.temporal.api.common.v1.WorkflowType;
+import io.temporal.api.enums.v1.WorkflowExecutionStatus;
+import io.temporal.api.workflow.v1.WorkflowExecutionInfo;
+import io.temporal.api.workflowservice.v1.ListWorkflowExecutionsRequest;
+import io.temporal.api.workflowservice.v1.ListWorkflowExecutionsResponse;
+import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowOptions;
+import io.temporal.client.WorkflowStub;
+import io.temporal.serviceclient.WorkflowServiceStubs;
+import io.temporal.worker.Worker;
+import io.temporal.worker.WorkerFactory;
+import io.temporal.workflow.*;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+public class SearchWorkFlow {
+
+    private static String TASK_QUEUE = "TASK_QUEUE";
+
+    @WorkflowInterface
+    public interface searchWorkflow {
+
+        @WorkflowMethod
+        public Boolean initiateWorkFlow();
+    }
+
+    public static class searchWorkflowImp implements searchWorkflow {
+
+        private final testActivity activities = Workflow.newActivityStub(testActivity.class, ActivityOptions.newBuilder().setStartToCloseTimeout(Duration.ofSeconds(20)).build());
+
+        @Override
+        public Boolean initiateWorkFlow() {
+            /* Use side-effect
+                *  Workflow code that includes arbitrary side effects (for example getting a random number or generating a random UUID, etc), can cause unpredictable results during replay.
+                *  Being able to add some non-deterministic code inside your Workflow is in some cases important, and you can do that using Workflow.sideEffect.
+
+                String randomString = Workflow.sideEffect(String.class, () -> generateRandom());
+            * */
+
+            Promise<Boolean> p = Async.function(activities::activityOne);
+            Boolean response = p.get();
+
+            return true;
+        }
+    }
+
+    @ActivityInterface
+    public interface testActivity {
+
+        public Boolean activityOne();
+
+
+    }
+
+    public static class testActivityImp implements testActivity {
+
+        @Override
+        public Boolean activityOne() {
+            try {
+                Thread.sleep(5000);
+                System.out.println("Activity  running for 5000ms");
+            } catch (Exception e) {
+                throw Activity.wrap(e);
+            }
+
+            return true;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        /* returns a string to be used as the WORKFLOW ID */
+        String randomString = generateRandom();
+
+        /* Worker code */
+        WorkflowServiceStubs service = WorkflowServiceStubs.newInstance();
+        WorkflowClient client = WorkflowClient.newInstance(service);
+        WorkerFactory factory = WorkerFactory.newInstance(client);
+        Worker worker = factory.newWorker(TASK_QUEUE);
+        worker.registerWorkflowImplementationTypes(searchWorkflowImp.class);
+        worker.registerActivitiesImplementations(new testActivityImp());
+        factory.start();
+
+        /* Set Workflow ID to the randomString */
+        WorkflowOptions options = WorkflowOptions.newBuilder().setTaskQueue(TASK_QUEUE).setWorkflowId(randomString).build();
+        searchWorkflow workflow = client.newWorkflowStub(searchWorkflow.class, options);
+
+        /*
+            Trigger the workflow async but wait for it to complete
+            In this case, since the workflow is async, the below code will make the workflow to complete
+         */
+        WorkflowExecution we = WorkflowClient.start(workflow::initiateWorkFlow);
+        WorkflowStub untyped = WorkflowStub.fromTyped(workflow);
+        Boolean workFLowStatus = untyped.getResult(Boolean.class);
+        System.out.println("    WORKFLOW STATUS: " + workFLowStatus);
+
+        /* Get the Workflow ID and Run ID */
+        String workFlowId = we.getWorkflowId();
+        String runId = we.getRunId();
+        System.out.println("    WORKFLOW ID: " + workFlowId + " RUN ID: " + runId);
+
+
+        /* Search for workFlows by the workFlowId and get meta-data */
+        String query = "WorkflowId='" + workFlowId + "'";
+        ListWorkflowExecutionsRequest wfeRequest = ListWorkflowExecutionsRequest.newBuilder().setQuery(query).setNamespace("default").build();
+        ListWorkflowExecutionsResponse wfeResponse = service.blockingStub().listWorkflowExecutions(wfeRequest);
+        List<WorkflowExecutionInfo> workFlowExecutionList = wfeResponse.getExecutionsList();
+
+        for (WorkflowExecutionInfo workFlowExecution : workFlowExecutionList) {
+            String workFlowType = workFlowExecution.getType().getName();
+            String workFlowStatus = workFlowExecution.getStatus().toString();
+            System.out.println("    WORKFLOW ID: " + workFlowId + " WORKFLOW STATUS: " + workFlowStatus);
+        }
+
+    }
+
+
+    /* Generic function that returns a random string */
+    public static String generateRandom() {
+
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+
+        int length = 7;
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(alphabet.length());
+            char randomChar = alphabet.charAt(index);
+            sb.append(randomChar);
+        }
+
+        return sb.toString();
+    }
+}
