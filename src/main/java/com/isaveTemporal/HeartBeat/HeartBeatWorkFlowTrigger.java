d29a90f41a8a307e7492fb4b93090c7511d5c86d
ajikee1 <akeeri1@students.towson.edu>
2021-12-31 00:40:31 -0500

    heart beat use case added

--- src/main/java/com/isaveTemporal/HeartBeat/HeartBeatWorkFlowTrigger.java
+++ src/main/java/com/isaveTemporal/HeartBeat/HeartBeatWorkFlowTrigger.java
@@ -9,17 +9,18 @@ import io.temporal.serviceclient.WorkflowServiceStubs;
 Use case:
     Demonstrates how heartbeats can be used to notify temporal server of activity progress.
 
-    Demonstrates how heartbeats 'last heartbeats details' can be used to handle activity failures on re-try.
+    Demonstrates how heartbeats 'last heartbeat details' can be used to handle activity failures on re-try.
     1. In activity 'heartBeatActivity' method, the code loops with 'i' 0 to 10, with the value of 'i' send back to temporal server as the heartbeat.
-    2. When i = 3, the code throws an exception, demonstrating an activity failure.
-    - This sets the last heartbeat detail to '3' (the latest heartbeat value when the failure happened)
-    - This last heartbeat detail is available through Activity.getExecutionContext().getHeartbeatDetails()
+    2. When i = 3, the code is forced to throw an exception, demonstrating an activity failure.
+    - This sets the 'last heartbeat detail' to '3' (the latest heartbeat value when the failure happened)
+    - This 'last heartbeat detail' is available through Activity.getExecutionContext().getHeartbeatDetails()
     3. Temporal will retry the activity by default
-    - On retry, the value of 'Activity.getExecutionContext().getHeartbeatDetails()' (3 in this case) is used to set the loop start to '4'
+    - On retry, the value of 'Activity.getExecutionContext().getHeartbeatDetails()' (3 in this case) is used to set the loop starting point to '4'
       and continue with 'i' 4 to 10.
-    - Thereby, using heartbeat to skip the failure point on re-try.
+    - Thereby, using heartbeat to skip the failure point on activity re-try.
 
  */
+
 public class HeartBeatWorkFlowTrigger {
 
     private static String TASK_QUEUE = "TASK_QUEUE";
